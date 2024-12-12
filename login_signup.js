8c1b83fd08c9079fe242840a553e5d91083f8362
Vaibhav S H <vaibhavsh.official@gmail.com>
2024-12-12 15:05:06 +0530

    1st commit

---
+++ login_signup.js
@@ -0,0 +1,9 @@
+function switchToLogin() {
+    document.getElementById('signup-form').style.display = 'none';
+    document.getElementById('login-form').style.display = 'block';
+}
+
+function switchToSignup() {
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('signup-form').style.display = 'block';
+}
